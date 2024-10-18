8de2510b6521764bfb4db8e9b82d0f6578d35653
BHandrick <150145759+BHandrick@users.noreply.github.com>
2024-10-18 10:29:33 +0200

    Anpassung Schriftart
    Artefakte beseitigt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+
 
 const Header = () => {
   return (
     <>
-    <div className="logo">
+      <div className="logo">
         <img src="src/assets/Weltauge_Farbanpassung_ohneSchrift.png" alt="Logo GeospatialAnalyzer" />
-    </div>
-    <div className='textGesamt'>
-        <div className="textG">Geospatial</div>
-        <div className="textA">Analyzer</div>
+      </div>
+      <div className='textGesamt'>
+        <div className="textG cormorant-logoFontG">Geospatial</div>
+        <div className="textA cormorant-logoFontA">Analyzer</div>
         <div className="textD"> - Demo Client</div>
 
-    </div>
-    
+      </div>
+
     </>
   );
 }
